d8cacb10dc0de1ad2d51bd5e109c20f795c64fb7
abhinavppradeep <abhinavppradeep2@gmail.com>
2024-06-29 12:54:53 +0530

    two sum hashing

---
+++ two sum hashing.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mpp;
+        for(int i=0;i<nums.size();i++){
+            int num = nums[i];
+            int moreneeded=target-num;
+            if(mpp.find(moreneeded) !=mpp.end()){
+                return {mpp[moreneeded],i };
+            }
+            mpp[num]=i;
+        }
+        return {};
+    }
+};
